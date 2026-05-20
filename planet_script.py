69d26e22139782187c8233db9c2dd45615eb989f
aaditiyatimetraveller <aditya.elfsolvers@gmail.com>
2026-05-20 20:32:31 +0530

    Update planet_script.py

--- planet_script.py
+++ planet_script.py
@@ -1,9 +1,46 @@
+import os
+import json
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 import pytz
 
-tz = pytz.timezone('Asia/Kolkata')
+# GitHub Secret पढ़ना
+creds_json = json.loads(os.environ['GOOGLE_CREDENTIALS'])
+
+with open("creds.json","w") as f:
+    json.dump(creds_json,f)
+
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/spreadsheets',
+'https://www.googleapis.com/auth/drive'
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "creds.json",
+    scope
+)
+
+client = gspread.authorize(creds)
+
+sheet_id = os.environ['SHEET_ID']
+
+sheet = client.open_by_key(sheet_id)
+
+worksheet = sheet.sheet1
+
+tz = pytz.timezone("Asia/Kolkata")
+
 now = datetime.now(tz)
 
-print("Mumbai Time:")
-print(now)
-print("GitHub Working Successfully")
+data = [
+    ["Time","Status"],
+    [now.strftime("%d-%m-%Y %H:%M:%S"),"Connected Successfully"]
+]
+
+worksheet.clear()
+worksheet.update("A1",data)
+
+print("Google Sheet Updated")
